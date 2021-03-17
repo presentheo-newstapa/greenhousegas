08f45ed8352b8df6ed0ed049af567f08950e3259
jae18067 <jae18067@gmail.com>
2021-03-17 18:27:02 +0900

    이미지 변경, 차트 추가

--- js/animation.js
+++ js/animation.js
@@ -36,19 +36,25 @@ animationTimeline.add({
 // 숫자 세기
 let foo = document.querySelectorAll('.spec')[0]
 let foo2 = document.querySelectorAll('.spec')[1]
-let foo3 = document.querySelectorAll('.spec')[3]
+let foo3 = document.querySelectorAll('.spec')[2]
+let foo4 = document.querySelectorAll('.spec')[3]
 
-var numAnim = new countUp.CountUp(foo, 80597292, {
+var numAnim = new countUp.CountUp(foo, 566814400, {
   duration: 3
 });
 numAnim.start()
 
-var numAnim2 = new countUp.CountUp(foo2, 170764602086, {
+var numAnim2 = new countUp.CountUp(foo2, 566814400*29126, {
   duration: 3
 });
 numAnim2.start()
 
-var numAnim3 = new countUp.CountUp(foo3, 1707646286, {
+var numAnim3 = new countUp.CountUp(foo3, 15597444142176, {
   duration: 3
 });
 numAnim3.start()
+
+var numAnim4 = new countUp.CountUp(foo4, 15597444142176, {
+  duration: 3
+});
+numAnim4.start()
